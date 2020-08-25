58aa2812bff6f5423b9d02fcb332ecd5cc4247db
aathithiyan <70151230+aathithiyan@users.noreply.github.com>
2020-08-25 09:43:54 +0530

    new java demo

---
+++ src/main/java/com/example/java/project/demo/java/project/DemoJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.java.project.demo.java.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java/project/demo/java/project/DemoJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java.project.demo.java.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
